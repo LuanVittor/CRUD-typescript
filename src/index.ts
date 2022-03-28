9d4dab01beac0ba68f186627eae3a8288d617bec
Luan Vitor <luan_vittor@hotmail.com>
2022-03-28 20:48:47 -0300

    starting ex 1

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import app from './app';
 
 const PORT = 3000;
 
-app.get('/produts', )
+app.get('/produts',)
 
 const server = app.listen(PORT, () => console.log(
   `Server is running on PORT: ${PORT}`,
