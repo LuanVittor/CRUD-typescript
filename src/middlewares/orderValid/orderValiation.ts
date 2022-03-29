4c6fe48238ac9a45a8133acdeefcad6f1a02d201
Luan Vitor <luan_vittor@hotmail.com>
2022-03-29 20:58:23 -0300

    All done

--- src/middlewares/orderValid/orderValiation.ts
+++ src/middlewares/orderValid/orderValiation.ts
@@ -2,12 +2,14 @@ import { Request, Response, NextFunction } from 'express';
 
 const productValid = (req: Request, res: Response, next: NextFunction) => {
   const { products } = req.body;
-  if (!products) return res.status(400).json({ error: 'Products is required' });
-  if (typeof products[0] !== 'number') {
-    return res.status(422).json({ error: 'Products must be an array of numbers' });
+  if (!products) {
+    return res.status(400).json({ error: 'Products is required' });
   }
   if (products.length === 0) {
     return res.status(422).json({ error: 'Products can\'t be empty' });
+  }
+  if (typeof products[0] !== 'number') {
+    return res.status(422).json({ error: 'Products must be an array of numbers' });
   }
   next();
 };
